5461f5e9106cec8b9e5de9d62da6712ceceb5d01
Undying-yueyue <orgtech007@gmail.com>
2021-10-06 02:56:33 +0000

    include: linux: Add task_is_booster() function
    It fixes this:
    
    kernel/sched/cpufreq_schedutil.c:681:5: error: implicit declaration of function 'task_is_booster' [-Werror,-Wimplicit-function-declaration]
            if(task_is_booster(current))
               ^
    ../../../../../../kernel/xiaomi/violet/kernel/sched/cpufreq_schedutil.c:681:5: note: did you mean 'task_is_zygote'?
    ../../../../../../kernel/xiaomi/violet/include/linux/binfmts.h:142:13: note: 'task_is_zygote' declared here
    extern bool task_is_zygote(struct task_struct *p);
                ^
    ../../../../../../kernel/xiaomi/violet/kernel/sched/cpufreq_schedutil.c:704:5: error: implicit declaration of function 'task_is_booster' [-Werror,-Wimplicit-function-declaration]
            if(task_is_booster(current))
               ^
    2 errors generated.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -678,7 +678,7 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if(task_is_booster(current))
+	if (task_is_booster(current))
 		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
@@ -701,7 +701,7 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if(task_is_booster(current))
+	if (task_is_booster(current))
 		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
